6d74627a8cefe38ec493b847c67d23f77fd5bb84
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-04-16 10:07:06 +0200

    creacion archivo conexion

---
+++ proyectoFinal/MODELO/conexion.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
+$db = mysql_connect("DB_HOST","DB_USER","DB_PASSWORD","DB_NAME","DB_PORT");
+
+?>
