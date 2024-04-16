9757295496d0715774b9b6d74a545227eacea03e
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-04-16 12:04:16 +0200

    cambios

--- proyectoFinal/MODELO/conexion.php
+++ proyectoFinal/MODELO/conexion.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 $DB_HOST = $_ENV["DB_HOST"];
 $DB_USER = $_ENV["DB_USER"];
@@ -7,5 +6,4 @@ $DB_PASSWORD = $_ENV["DB_PASSWORD"];
 $DB_NAME = $_ENV["DB_NAME"];
 $DB_PORT = $_ENV["DB_PORT"];
 $db = mysql_connect("DB_HOST","DB_USER","DB_PASSWORD","DB_NAME","DB_PORT");
-
 ?>
--- proyectoFinal/index.php
+++ proyectoFinal/index.php
@@ -1,2 +1,4 @@
 <?php
+include_once "/app/MODELO/conexion.php";
 header("Location: ./vista/Inicio_Sesion.php");
+?>
