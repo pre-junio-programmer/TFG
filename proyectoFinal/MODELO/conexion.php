678df6d9d4799f119ce2bf161e4558f3399d7f23
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-04-17 18:53:10 +0200

    cambios

--- proyectoFinal/MODELO/conexion.php
+++ proyectoFinal/MODELO/conexion.php
@@ -5,5 +5,5 @@ $DB_USER = $_ENV["DB_USER"];
 $DB_PASSWORD = $_ENV["DB_PASSWORD"];
 $DB_NAME = $_ENV["DB_NAME"];
 $DB_PORT = $_ENV["DB_PORT"];
-$db = mysql_connect("DB_HOST","DB_USER","DB_PASSWORD","DB_NAME","DB_PORT");
+$db = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 ?>
