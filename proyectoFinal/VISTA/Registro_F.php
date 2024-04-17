2aeaf68d4e4446fff0fd79c85e4e3e601d016c6a
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-04-17 19:23:22 +0200

    f

--- proyectoFinal/VISTA/Registro_F.php
+++ proyectoFinal/VISTA/Registro_F.php
@@ -12,23 +12,21 @@ include_once "/app/MODELO/conexion.php";
 <body>
     <h2>Registro de Usuario</h2>
     <form action="../CONTROLADOR/Registro_Correcto.php" method="post">
-        <label for="id_usuario">ID Usuario:</label>
-        <input type="text" id="id_usuario" name="id_usuario" required><br><br>
         
         <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required><br><br>
+        <input type="text" id="nombre" name="nombre"><br><br>
         
         <label for="direccion">Dirección:</label>
-        <input type="text" id="direccion" name="direccion" required><br><br>
+        <input type="text" id="direccion" name="direccion"><br><br>
         
         <label for="correo">Correo electrónico:</label>
-        <input type="text" id="correo" name="correo" required><br><br>
+        <input type="text" id="correo" name="correo"><br><br>
         
         <label for="contraseña">Contraseña:</label>
-        <input type="password" id="contraseña" name="contraseña" required><br><br>
+        <input type="password" id="contraseña" name="contraseña"><br><br>
         
         <label for="saldo">Saldo:</label>
-        <input type="number" id="saldo" name="saldo" step="0.01" required><br><br>
+        <input type="number" id="saldo" name="saldo"><br><br>
         
         <input type="submit" value="Registrar Usuario">
     </form>
