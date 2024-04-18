ca992ee009b3d7cb2553fe85743dddeaf334ba65
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-04-18 18:45:02 +0200

    c

--- proyectoFinal/VISTA/Inicio_Sesion_F.php
+++ proyectoFinal/VISTA/Inicio_Sesion_F.php
@@ -1,5 +1,14 @@
 <?php
 include_once "/app/MODELO/conexion.php";
+
+// Verificar si hay un mensaje de éxito en la sesión
+if (isset($_SESSION['mensaje'])) {
+    $mensaje = $_SESSION['mensaje'];
+    // Eliminar el mensaje de la sesión para que no se muestre más de una vez
+    unset($_SESSION['mensaje']);
+} else {
+    $mensaje = ""; // Si no hay mensaje, establecer como cadena vacía
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,15 +20,37 @@ include_once "/app/MODELO/conexion.php";
 </head>
 <body>
     <h2>Inicio de Sesion</h2>
-    //<form action="../CONTROLADOR/Registro_Correcto.php" method="post">
+    <!-- Mostrar mensaje de éxito si existe -->
+    <?php if ($mensaje != ""): ?>
+        <p><?php echo $mensaje; ?></p>
+    <?php endif; ?>
+    <form action="../CONTROLADOR/Registro_Correcto.php" method="post">
         
         <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre"><br><br>
+        <input type="text" id="nombre" name="nombre">
+        
+        <br><br>
         
         <label for="contraseña">Contraseña:</label>
-        <input type="password" id="contraseña" name="contraseña"><br><br>
+        <input type="password" id="contraseña" name="contraseña"> 
+        <!-- Agregamos un elemento de imagen que se puede hacer clic para mostrar la contraseña -->
+        <img src="../imagenes/ojo.png" id="mostrarContrasena" width="30px" height="30px" style="cursor: pointer;">
+        
+        <br><br>
         
         <input type="submit" value="Iniciar Sesion">
     </form>
+    
+    <script>
+        function alternarTipoContraseña() {
+            var campoContraseña = document.getElementById("contraseña");
+            if (campoContraseña.type === "password") {
+                campoContraseña.type = "text";
+            } else {
+                campoContraseña.type = "password";
+            }
+        }
+        document.getElementById("mostrarContrasena").onclick(alternarTipoContraseña);
+    </script>
 </body>
 </html>
