639e8f191366534381543ff87b36a661d5010d21
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-04-20 11:52:46 +0200

    b

--- proyectoFinal/VISTA/Registro_F.php
+++ proyectoFinal/VISTA/Registro_F.php
@@ -21,21 +21,45 @@ if (!empty($mensajeError_campos)) {
     <form action="../CONTROLADOR/Registro_Correcto.php" method="post">
         
         <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre"><br><br>
+        <input type="text" id="nombre" name="nombre">
+        
+        <br><br>
         
         <label for="direccion">Dirección:</label>
-        <input type="text" id="direccion" name="direccion"><br><br>
+        <input type="text" id="direccion" name="direccion">
+        
+        <br><br>
         
         <label for="correo">Correo electrónico:</label>
-        <input type="text" id="correo" name="correo"><br><br>
+        <input type="text" id="correo" name="correo">
+        
+        <br><br>
         
         <label for="contraseña">Contraseña:</label>
-        <input type="password" id="contraseña" name="contraseña"><br><br>
+        <input type="password" id="contraseña" name="contraseña">
+
+        <img src="../imagenes/ojo.png" id="mostrar" width="30px" height="30px" style="cursor: pointer;">
+        
+        <br><br>
         
         <label for="saldo">Saldo:</label>
-        <input type="number" id="saldo" name="saldo"><br><br>
+        <input type="number" id="saldo" name="saldo">
+        
+        <br><br>
         
         <input type="submit" value="Registrar Usuario">
     </form>
+
+    <script>
+        function click_imagen() {
+            var campoContrasena = document.getElementById("contraseña");
+            if (campoContrasena.type === "password") {
+                campoContrasena.type = "text";
+            } else {
+                campoContrasena.type = "password";
+            }
+        }
+        document.getElementById("mostrar").addEventListener("click", click_imagen);
+    </script>
 </body>
 </html>
