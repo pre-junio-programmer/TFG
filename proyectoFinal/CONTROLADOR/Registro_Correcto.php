1853ad158bb6530fe50b737ccc773a92f7c899ed
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-04-20 11:18:56 +0200

    h

--- proyectoFinal/CONTROLADOR/Registro_Correcto.php
+++ proyectoFinal/CONTROLADOR/Registro_Correcto.php
@@ -5,9 +5,9 @@ $resultado = Operaciones_Base::insertarUsuario($_POST['nombre'], $_POST['correo'
 
 if ($resultado=="Todo_Correcto") {
     //PODEMOS CAMBIARLO A LA PAG PRINCIPAL EN VEZ DE AL INICIO
-    header("Location:../VISTA/Inicio_Sesion_F.php?error=Usuario creado con éxito.");
+    header("Location:../VISTA/Inicio_Sesion_F.php?error=Usuario creado con éxito");
 }else if($resultado=="Campo_Vacío"){
-    header("Location:../VISTA/Registro_F.php?error=Rellena todos los campos por favor.");
+    header("Location:../VISTA/Registro_F.php?error=Rellena todos los campos por favor");
 }else if($resultado=="Nombre_Existente"){
     header("Location:../VISTA/Registro_F.php?error=El nombre que has introducido ya está en uso");
 }else{
