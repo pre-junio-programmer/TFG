7e73fe76b19323bcea290330f59b0055697398a5
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-04-21 10:56:39 +0200

    intento mezcla de react y php 8

--- proyecto_final/index.php
+++ proyecto_final/index.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Registro de Usuario</title>
-</head>
-<body>
-    <h2>Registro de Usuario</h2>
-
-    <!-- Aquí incluyes tu componente React -->
-    <div id="registro-form-container"></div>
-
-    <!-- Incluye el script de React -->
-    <script src="src/App.js"></script>
-
-    <!-- Aquí incluye tu script de React que renderiza el componente -->
-    <script>
-        // Renderiza el componente React dentro del contenedor con id "registro-form-container"
-        ReactDOM.render(
-            <Registro_F />,
-            document.getElementById('registro-form-container')
-        );
-    </script>
-</body>
-</html>
+<?php
+header("Location: src/VISTA/Registro_F.php");
+?>
