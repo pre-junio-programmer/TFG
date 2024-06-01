97e51f3de9e2a633563d1278359634da3b074ed9
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-06-01 19:15:43 +0200

    sin boton comprar

---
+++ tfg_def/TFG/MODELO/Manejo_Base.php
@@ -0,0 +1,294 @@
+<?php
+class Base_Operaciones {
+    
+    public static function conexion() {
+        try {
+            $conexion = new PDO('mysql:host=localhost; dbname=tfg_base_def', 'root', '');
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conexion->exec("SET CHARACTER SET UTF8");
+        } catch (Exception $e) {
+            echo "Linea de error: " . $e->getLine();
+            die("Erro" . $e->getMessage());
+        }
+        return $conexion;
+    }
+
+    public static function seleccionarValor($valorBuscar,$valorSeleccionar,$campo,$tabla) {
+        $conexion = Base_Operaciones::conexion();
+        $buscarValor = "SELECT {$valorSeleccionar} FROM {$tabla} WHERE {$campo} = :valor";
+        $ejecutarValor = $conexion->prepare($buscarValor);
+        $ejecutarValor->bindValue(":valor", $valorBuscar);
+        $ejecutarValor->execute();
+        $resultado = $ejecutarValor->fetch(PDO::FETCH_ASSOC);
+    
+        if ($resultado) {
+            return $resultado[$valorSeleccionar];
+        } else {
+            return null; 
+        }    
+    }
+
+
+
+    public static function comprobarCampoUnico($valor,$campo) {
+        $conexion = Base_Operaciones::conexion();
+
+        $buscarCampo = "SELECT COUNT(*) as count FROM usuario WHERE {$campo} = :valor";
+        $ejecutarCampo = $conexion->prepare($buscarCampo);
+        $ejecutarCampo->bindValue(":valor", $valor);
+        $ejecutarCampo->execute();
+    
+        $existeCampo = $ejecutarCampo->fetch(PDO::FETCH_ASSOC);
+    
+        
+        return $existeCampo['count'] > 0;
+       
+    }
+    
+
+    public static function obtenerUltimoIdUsuario() {
+        $conexion = Base_Operaciones::conexion();
+        
+            $sql = "SELECT MAX(id) AS ultimo_id FROM users";
+            $consulta = $conexion->query($sql);
+            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+            $ultimoId = $resultado['ultimo_id'];
+            return $ultimoId;
+     
+    }
+    public static function insertarUsuario($nombreUsu, $correoUsu, $contraUsu, $pintorSelect) {
+        $conexion = Base_Operaciones::conexion();
+    
+    
+        $nombreNoExistente = Base_Operaciones::comprobarCampoUnico($nombreUsu,"name");
+        $emailNoExistente = Base_Operaciones::comprobarCampoUnico($correoUsu,"email");
+        $idMaximo = Base_Operaciones::obtenerUltimoIdUsuario();
+        $idNuevo = $idMaximo+1;
+        $pintor =  intval($pintorSelect);
+
+        if($nombreUsu=="" || $contraUsu=="" || $correoUsu=="" || $pintorSelect==""){
+            $resultado = "B";
+            return $resultado;
+        }
+        else if($nombreNoExistente) {
+            $resultado = "A";
+            return $resultado;
+        } else if($emailNoExistente) {
+            $resultado = "D";
+            return $resultado;
+        } else {
+            
+            $sql = "INSERT INTO users(id,name,password,email,painter_fk) VALUES (:id,:usuario,:contrasena,:correo,:pintor)";
+            $resultado = $conexion->prepare($sql);
+    
+            $resultado->bindValue(":usuario", $nombreUsu);
+            $resultado->bindValue(":contrasena", $contraUsu);
+            $resultado->bindValue(":correo", $correoUsu);
+            $resultado->bindValue(":pintor", $pintor);
+            $resultado->bindValue(":id", $idNuevo);
+    
+    
+            
+            $resultado->execute();
+    
+            $resultado = "C";
+            return $resultado;
+        }
+    }
+    public static function inicioExitoso($nombreIntroducido, $contraIntroducida) {
+        $conexion = Base_Operaciones::conexion();
+        
+        $sql = "SELECT * FROM usuario WHERE nombre_u = :nombreUsu AND contra_u = :contraUsu";
+        $resultado = $conexion->prepare($sql);
+        $resultado->bindValue(":nombreUsu", $nombreIntroducido);
+        $resultado->bindValue(":contraUsu", $contraIntroducida);
+        $resultado->execute();
+        
+        if($resultado->rowCount() > 0) {
+            session_start();
+            $_SESSION['nombreDeSesion'] = $nombreIntroducido;
+            return true;
+        } else {
+            return false; 
+        }
+    }
+
+    public static function mostrarProductos($categoria) {
+    $conexion = Base_Operaciones::conexion();
+    $sql = "SELECT id_producto, nombre_p, descripcion_p FROM producto WHERE categoria_p = :categoria";
+    $consultaProducto = $conexion->prepare($sql);
+    $consultaProducto->bindParam(":categoria", $categoria);
+    $consultaProducto->execute();
+
+    $productos = $consultaProducto->fetchAll(PDO::FETCH_ASSOC);
+    
+    return $productos;
+    }
+
+
+    public static function mostrarDatosCuadro($numeroCuadro) {
+        $conexion = Base_Operaciones::conexion();
+
+        $buscarCuadro = "SELECT * FROM paintings WHERE id = :numeroCuadro";
+        $cuadroObtenido = $conexion->prepare($buscarCuadro);
+        $cuadroObtenido->bindParam(":numeroCuadro", $numeroCuadro);
+        $cuadroObtenido->execute();
+        $devolver = $cuadroObtenido->fetch(PDO::FETCH_ASSOC);
+
+        return $devolver;
+    }
+
+    public static function borrarUsuario($nombreUsuario,$contraUsu) {
+        $conexion = Base_Operaciones::conexion();
+    
+        $borrarTodo = "DELETE FROM users WHERE name = :nombreUsu and password=:contraUsu";
+        $borrarDefinitivo = $conexion->prepare($borrarTodo);
+        $borrarDefinitivo->bindValue(":nombreUsu", $nombreUsuario);
+        $borrarDefinitivo->bindValue(":contraUsu", $contraUsu);
+
+        $borrarDefinitivo->execute();
+    }
+    public static function obtenerDatos($nombreUsuario) {
+        $conexion = Base_Operaciones::conexion();
+        $buscarUsuario = "SELECT name,password,email,painter_fk FROM users WHERE name = :nombreUsuario";
+        $usuarioObtenido = $conexion->prepare($buscarUsuario);
+        $usuarioObtenido->bindParam(":nombreUsuario", $nombreUsuario);
+        $usuarioObtenido->execute();
+        $arrayUsuario = $usuarioObtenido->fetch(PDO::FETCH_ASSOC);
+    
+        return $arrayUsuario;
+    }
+    
+    public static function comprobarCampoUnicoModificacion($valor, $campo,$variableSesion) {
+        $conexion = Base_Operaciones::conexion();
+    
+        $buscarCampo = "SELECT COUNT(*) as count FROM users WHERE {$campo} = :valor AND name != :variableSesion";
+        $ejecutarCampo = $conexion->prepare($buscarCampo);
+        $ejecutarCampo->bindValue(":valor", $valor);
+        $ejecutarCampo->bindValue(":variableSesion", $variableSesion);
+        $ejecutarCampo->execute();
+    
+        $existeCampo = $ejecutarCampo->fetch(PDO::FETCH_ASSOC);
+    
+        return $existeCampo['count'] > 0;
+    }
+
+    public static function modificarUsuario($nombreMod, $correoMod, $contraMod, $pintorSelMod, $nombreSesion) {
+        $conexion = Base_Operaciones::conexion();
+
+        $nombreNoRepetido = Base_Operaciones::comprobarCampoUnicoModificacion($nombreMod,"name",$nombreSesion);
+        $emailNoRepetido = Base_Operaciones::comprobarCampoUnicoModificacion($correoMod,"email",$nombreSesion);
+       
+
+
+        if($nombreMod=="" || $contraMod=="" || $correoMod=="" || $pintorSelMod==""){
+            $resultado = "B";
+            return $resultado;
+        }
+        else if($nombreNoRepetido) {
+            $resultado = "A";
+            return $resultado;
+        } else if($emailNoRepetido) {
+            $resultado = "D";
+            return $resultado;
+        } else {
+            
+            $sql = "UPDATE users SET name = :nombreMod, password = :contraMod, email = :correoMod, painter_fk = :pintorMod WHERE name = :nombreNoBuscar";
+            $resultado = $conexion->prepare($sql);
+    
+            $resultado->bindValue(":nombreMod", $nombreMod);
+            $resultado->bindValue(":contraMod", $contraMod);
+            $resultado->bindValue(":correoMod", $correoMod);
+            $resultado->bindValue(":pintorMod", $pintorSelMod);
+            $resultado->bindValue(":nombreNoBuscar", $nombreSesion);
+        
+            $resultado->execute();
+            $_SESSION['nombreDeSesion']=$nombreMod; 
+    
+            $resultado = "C";
+            return $resultado;
+        }
+    }
+
+    public static function obtenerProductoPorId($id_producto) {
+        $conexion = Base_Operaciones::conexion();
+    
+        $sql = "SELECT nombre_p, descripcion_p, precio_p, cantidad_p FROM producto WHERE id_producto = :id_producto";
+        $consultaProducto = $conexion->prepare($sql);
+        $consultaProducto->bindParam(":id_producto", $id_producto);
+        $consultaProducto->execute();
+    
+        $producto = $consultaProducto->fetch(PDO::FETCH_ASSOC);
+    
+        return $producto;
+    }
+
+    public static function obtenerComentariosPorProducto($id_producto) {
+        $conexion = Base_Operaciones::conexion();
+        
+        $sql = "SELECT c.id_comentario, u.nombre_u AS nombre_usuario, c.valoracion_c, c.comentario_c 
+                FROM Comentario c
+                JOIN Relacion_Comentario rc ON c.id_comentario = rc.id_comentario
+                JOIN Usuario u ON rc.id_usuario = u.id_usuario
+                WHERE rc.id_producto = :id_producto";
+        
+        $consultaComentario = $conexion->prepare($sql);
+        $consultaComentario->bindParam(":id_producto", $id_producto);
+        $consultaComentario->execute();
+        
+        $comentarios = $consultaComentario->fetchAll(PDO::FETCH_ASSOC);
+        
+        return $comentarios;
+    }
+    
+
+    public static function eliminarComentario($id_comentario) {
+        $conexion = Base_Operaciones::conexion();
+    
+        try {
+
+            $conexion->beginTransaction();
+    
+
+            $sqlRelacion = "DELETE FROM Relacion_Comentario WHERE id_comentario = :id_comentario";
+            $consultaRelacion = $conexion->prepare($sqlRelacion);
+            $consultaRelacion->bindParam(":id_comentario", $id_comentario);
+            $consultaRelacion->execute();
+    
+
+            $sqlComentario = "DELETE FROM Comentario WHERE id_comentario = :id_comentario";
+            $consultaComentario = $conexion->prepare($sqlComentario);
+            $consultaComentario->bindParam(":id_comentario", $id_comentario);
+            $consultaComentario->execute();
+    
+
+            $conexion->commit();
+        } catch (Exception $e) {
+
+            $conexion->rollBack();
+            throw $e;
+        }
+    }
+
+    public static function obtenerComentariosPorProductoOrdenados($id_producto, $orden) {
+        $conexion = Base_Operaciones::conexion();
+        
+        $sql = "SELECT c.id_comentario, u.nombre_u AS nombre_usuario, c.valoracion_c, c.comentario_c 
+                FROM Comentario c
+                JOIN Relacion_Comentario rc ON c.id_comentario = rc.id_comentario
+                JOIN Usuario u ON rc.id_usuario = u.id_usuario
+                WHERE rc.id_producto = :id_producto
+                ORDER BY c.valoracion_c $orden";
+        
+        $consultaComentario = $conexion->prepare($sql);
+        $consultaComentario->bindParam(":id_producto", $id_producto);
+        $consultaComentario->execute();
+        
+        $comentarios = $consultaComentario->fetchAll(PDO::FETCH_ASSOC);
+        
+        return $comentarios;
+    }
+
+}
+
+?>
---
+++ tfg_def/TFG/VISTA/producto.php
@@ -0,0 +1,143 @@
+<?php
+session_start();
+require_once "../MODELO/Manejo_Base.php";
+
+if (isset($_GET['id'])) {
+    $id_producto = $_GET['id'];
+    $producto = Base_Operaciones::obtenerProductoPorId($id_producto);
+    $comentarios = Base_Operaciones::obtenerComentariosPorProducto($id_producto);
+
+    if ($producto) {
+        $nombre = $producto['nombre_p'];
+        $descripcion = $producto['descripcion_p'];
+        $precio = $producto['precio_p'];
+        $stock = $producto['cantidad_p'];
+
+        $formatos = ['jpeg', 'png', 'jpg'];
+        $ruta = '';
+
+        foreach ($formatos as $formato) {
+            $src = "../img/{$id_producto}.{$formato}";
+            if (file_exists($src)) {
+                $ruta = $src;
+                break;
+            }
+        }
+    } else {
+        echo "Producto no encontrado.";
+        exit;
+    }
+} else {
+    echo "ID de producto no especificado.";
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['orden']) && !empty($_POST['orden'])) {
+        $orden = $_POST['orden'];
+        if ($orden === 'mas-valorados') {
+
+            $comentarios = Base_Operaciones::obtenerComentariosPorProductoOrdenados($id_producto, 'desc');
+
+        } elseif ($orden === 'menos-valorados') {
+
+            $comentarios = Base_Operaciones::obtenerComentariosPorProductoOrdenados($id_producto, 'asc');
+
+        } else if($orden === 'orden-normal') {
+
+            $comentarios = Base_Operaciones::obtenerComentariosPorProducto($id_producto);
+
+        }
+
+    } elseif (isset($_POST['id_comentario']) && !empty($_POST['id_comentario'])) {
+
+        $id_comentario = $_POST['id_comentario'];
+        Base_Operaciones::eliminarComentario($id_comentario);
+        header("Location: producto.php?id=" . $id_producto);
+        exit();
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Producto</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="producto.js"></script>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="card" id="eliminar">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1 class="card-title"><?php echo htmlspecialchars($nombre); ?></h1>
+                        <p class="card-text"><?php echo htmlspecialchars($descripcion); ?></p>
+                        <h3 class="card-subtitle mb-2 text-muted">Precio: <?php echo htmlspecialchars($precio); ?> €</h3>
+                        <h4 class="card-subtitle mb-2 text-muted"><?php echo htmlspecialchars($stock); ?> disponibles</h4>
+                        <a href="#" class="btn btn-primary">Comprar</a>
+                        <br>
+                        <br>
+                        <form id="ordenForm" method="post" action="">
+                            <select id="orden" name="orden" class="form-control" onchange="cambiarOrden()">
+                                <option value="">Elegir orden</option>
+                                <option value="orden-normal">Predeterminado</option>
+                                <option value="mas-valorados">Más valorados</option>
+                                <option value="menos-valorados">Menos valorados</option>
+                            </select>
+                        </form>
+                    </div>
+                    <div class="col-md-4">
+                        <?php if ($ruta): ?>
+                            <img src="<?php echo htmlspecialchars($ruta); ?>" class="card-img-top img-fluid" alt="Imagen del producto">
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-5">
+            <div id="comentarios">
+                <h2><?php if ($comentarios): ?></h2>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Usuario</th>
+                                <th>Valoración</th>
+                                <th>Comentario</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($comentarios as $comentario): ?>
+                                <tr>
+                                    <td><?php echo htmlspecialchars($comentario['nombre_usuario']); ?></td>
+                                    <td><?php echo htmlspecialchars($comentario['valoracion_c']); ?></td>
+                                    <td><?php echo htmlspecialchars($comentario['comentario_c']); ?></td>
+                                    <?php if (isset($_SESSION['nombreDeSesion']) && $_SESSION['nombreDeSesion'] === $comentario['nombre_usuario']): ?>
+                                        <td>
+                                            <form method="post" action="">
+                                                <input type="hidden" name="id_comentario" value="<?php echo htmlspecialchars($comentario['id_comentario']); ?>">
+                                                <button type="submit" class="btn btn-danger">Eliminar</button>
+                                            </form>
+                                        </td>
+                                    <?php else: ?>
+                                        <td></td>
+                                    <?php endif; ?>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                <?php else: ?>
+                    <p>No hay comentarios para este producto.</p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
