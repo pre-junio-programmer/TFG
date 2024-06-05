286662b77969b95a37ac23c778d63cc3ed2ba0e7
LAPTOP-5AOM6B0G\Jorge <jorge.romero@crcit.es>
2024-06-05 19:54:43 +0200

    campo cantidad e imagenes

---
+++ tfg_2048/TFG/CONTROLADOR/Modificar_Usuario.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once "../MODELO/Manejo_Base.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nombre_usuario = $_SESSION['nombreDeSesion'];
+    $nombre = $_POST['nombre'];
+    $direccion = $_POST['direccion'];
+    $correo = $_POST['email'];
+    
+    $id_usuario = Base_Operaciones::seleccionarValor($nombre_usuario, 'id_usuario', 'nombre_u', 'usuario');
+    $respuesta = Base_Operaciones::updateUser($id_usuario,$nombre,$direccion,$correo);
+
+    if ($respuesta == "A") {
+        header("Location: ../VISTA/CambiarInformacionUsuario.html?error=1");
+        exit();
+    } else if ($respuesta == "B") {
+        header("Location: ../VISTA/CambiarInformacionUsuario.html?error=2");
+        exit();
+    } else {
+        header("Location: ../VISTA/PaginaPrincipal.html");
+        exit();
+    }
+}
+?>
